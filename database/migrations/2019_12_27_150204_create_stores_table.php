8065d51bb55cfd3837180a567bc92b9c4615e8df
omor faruk <abid1058omor@gmail.com>
2019-12-27 21:17:55 +0600

    Create Model and run database migration

---
+++ database/migrations/2019_12_27_150204_create_stores_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('key');
+            $table->text('value');
+            $table->timestamp('ttl')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
